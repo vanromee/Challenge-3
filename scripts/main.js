2ff3faf4075f97072304daaf7888ff50f3722744
vanromee <58208601+vanromee@users.noreply.github.com>
2020-06-10 23:56:40 +0200

    Display Weather API

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+
+
+function getAPIdata() {
+
+	var url = 'https://api.openweathermap.org/data/2.5/weather';
+	var apiKey ='f85bc440fac6d0317cee7070d886a3a9';
+	var city = 'den haag';
+
+	// construct request
+	var request = url + '?' + 'appid=' + apiKey + '&' + 'q=' + city; //form your request
+	
+	// get current weather
+	fetch(request)
+	
+	// parse to JSON format
+	.then(function(response) {
+		if(!response.ok) throw Error(response.statusText);
+		return response.json();
+	})
+	
+	// render weather per day
+	.then(function(response) {
+		// render weatherCondition
+		onAPISuccess(response);	
+		console.log(response);
+	});
+}
+
+
+function fromKtoC(kelvin){
+	var celsius = kelvin - 273.15;
+	return celsius.toFixed(1);
+}
+
+
+function onAPISuccess(response) {
+	console.log(response.main);
+
+	var city = response.name;
+	var temp = response.main.temp;
+	var feelsLike = response.main.feels_like;
+	var tempMax = response.main.temp_max;
+	var tempMin = response.main.temp_min;
+	var humidity = response.main.humidity;
+	var pressure = response.main.pressure;
+
+	document.getElementById('weather').innerHTML = city + '<br>' + fromKtoC(temp);
+}
+
+getAPIdata();
